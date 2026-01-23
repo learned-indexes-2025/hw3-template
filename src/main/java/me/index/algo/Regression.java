bb6e324d5196cf04af4d13019d396bb29b56ceb1
Grigoriy Khlytin <xlootin@niuitmo.ru>
2026-01-23 05:47:26 +0300

    add default regression + test

--- src/main/java/me/index/algo/Regression.java
+++ src/main/java/me/index/algo/Regression.java
@@ -3,6 +3,10 @@ package me.index.algo;
 import java.util.List;
 
 public class Regression implements Splittable {
+    // ----- store the current segment: -----
+    RadixSpline segment;
+    // --------------------------------------
+
     public Regression() {
     }
 
@@ -12,9 +16,19 @@ public class Regression implements Splittable {
         while (start < keys.size()) {
             int i = 0;
             double[] cf = new double[]{0.0, 0.0};
+            // ----- init: -----
+            segment = new RadixSpline(keys.get(start + i), maxErr);
+            i++;
+            // -----------------
             while (start + i < keys.size() && can_expand(keys.get(start + i), i, maxErr, cf)) {
                 i++;
             }
+            // ----- updating weights: -----
+            if (segment.done) {
+                cf[0] = (segment.y) / (segment.x);
+                cf[1] = -cf[0] * segment.key0;
+            }
+            // -----------------------------
             int err = 0;
             for (int j = start; j < start + i; j++)
                 err = Math.max(err, Math.abs(predict(cf[0], cf[1], keys.get(j)) - (j - start)));
@@ -29,11 +43,64 @@ public class Regression implements Splittable {
     }
 
     public boolean can_expand(long key, int pos, int maxErr, double[] cf) {
-        return true;
+        return segment.add(key, pos); // corrected
     }
 
     public static int predict(double k, double b, long x) {
         int pos = (int) (k * x + b);
         return Math.max(pos, 0);
     }
+
+    // ----- main: -----
+    public static class RadixSpline {
+        double key0;
+        long eps;
+        boolean done;
+
+        double x;
+        double y;
+        double ux;
+        double uy;
+        double lx;
+        double ly;
+
+        public RadixSpline(double key0, long eps) {
+            this.key0 = key0;
+            this.eps = eps;
+            this.done = false;
+        }
+
+        public boolean add(double key, double pos) {
+            if (!done) {
+                done = true;
+                x = (key - key0);
+                y = pos;
+                ux = x;
+                uy = y + eps;
+                lx = x;
+                ly = y - eps;
+                return true;
+            }
+            if (cross((key - key0), pos, ux, uy) > 0 &&
+                    cross((key - key0), pos, lx, ly) < 0) {
+                x = (key - key0);
+                y = pos;
+                if (cross(ux, uy, x, y + eps) < 0) {
+                    ux = x;
+                    uy = y + eps;
+                }
+                if (cross(lx, ly, x, y - eps) > 0) {
+                    lx = x;
+                    ly = y - eps;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static double cross(double ax, double ay, double bx, double by) {
+            return ax * by - bx * ay;
+        }
+    }
+    // -----------------
 }
---
+++ src/test/java/me/index/TestMain.java
@@ -0,0 +1,81 @@
+package me.index;
+
+import me.index.algo.Regression;
+import me.index.algo.Splittable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+    public final int SEED = 237;
+    public final int SIZE = (int) 1e7;
+
+    public int split_test_common(Splittable splittable, List<Long> keys, int maxErr, boolean strict) {
+        Holder<Integer> last = new Holder<>(0);
+        Holder<Integer> cnt = new Holder<>(0);
+        splittable.split(keys, maxErr, (start, end, lrm) -> {
+            cnt.v = cnt.v + 1;
+            assertEquals(last.v, start, "(start == 0)");
+            last.v = end;
+            int err = 0;
+            for (int i = start; i < end; i++) {
+                int p_pos = Math.max(0, (int) (lrm.k() * keys.get(i) + lrm.b()));
+                int a_pos = i - start;
+                err = Math.max(err, Math.abs(p_pos - a_pos));
+            }
+            assertTrue(err <= lrm.maxErr(), String.format("(err %d <= lrm.maxErr() %d)", err, lrm.maxErr()));
+            if (strict)
+                assertTrue(err <= maxErr, String.format("(err %d <= maxErr %d)", err, maxErr));
+        });
+        assertEquals(last.v, keys.size(), "(end == keys.size())");
+        return cnt.v;
+    }
+
+    // ---------- split_test weak: ----------
+
+    @Test
+    public void split_test_weak_linear() {
+        List<Long> L_KEYS_32 = Utils.generateLinearKeys(SIZE, false, new Random(SEED));
+        for (int maxErr = 1; maxErr <= 128; maxErr *= 2) {
+            System.out.println("[DEBUG] split_test weak linear32, err = " + maxErr + ", segments: " +
+                    split_test_common(new Regression(), L_KEYS_32, maxErr, false));
+        }
+    }
+
+    @Test
+    public void split_test_weak_uniform() {
+        List<Long> U_KEYS_32 = Utils.generateUniformKeys(SIZE, false, new Random(SEED));
+        for (int maxErr = 1; maxErr <= 128; maxErr *= 2) {
+            System.out.println("[DEBUG] split_test weak uniform32, err = " + maxErr + ", segments: " +
+                    split_test_common(new Regression(), U_KEYS_32, maxErr, false));
+        }
+    }
+
+    // ----- split_test strict: -----
+
+    @DisabledIfSystemProperty(named = "skip.strict", matches = "true")
+    @Test
+    public void split_test_strict_linear() {
+        List<Long> L_KEYS_64 = Utils.generateLinearKeys(SIZE, true, new Random(SEED));
+        for (int maxErr = 1; maxErr <= 128; maxErr *= 2) {
+            System.out.println("[DEBUG] split_test strict linear64, err = " + maxErr + ", segments: " +
+                    split_test_common(new Regression(), L_KEYS_64, maxErr, true));
+        }
+    }
+
+    @DisabledIfSystemProperty(named = "skip.strict", matches = "true")
+    @Test
+    public void split_test_strict_uniform() {
+        List<Long> U_KEYS_64 = Utils.generateUniformKeys(SIZE, true, new Random(SEED));
+        for (int maxErr = 1; maxErr <= 128; maxErr *= 2) {
+            System.out.println("[DEBUG] split_test strict uniform64, err = " + maxErr + ", segments: " +
+                    split_test_common(new Regression(), U_KEYS_64, maxErr, true));
+        }
+    }
+
+}
